bebfa9aa2c81baf4b5be611531755f95297f2951
Yu Myungjae <u4bi@letsee.io>
2017-05-23 16:55:46 +0900

    - 롤업 빌드 환경 설정

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import riot from 'rollup-plugin-riot'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import buble from 'rollup-plugin-buble'
+import postcss from 'postcss'
+import postcssCssnext from 'postcss-cssnext'
+
+export default {
+  entry: 'src/main.js',
+  dest: 'dist/bundle.js',
+  plugins: [
+    riot({
+      style: 'cssnext',
+      parsers: {
+        css: { cssnext }
+      }
+    }),
+    nodeResolve({ jsnext: true }),
+    commonjs(),
+    buble()
+  ],
+  format: 'iife'
+}
+
+function cssnext (tagName, css) {
+  css = css.replace(/:scope/g, ':root')
+  css = postcss([postcssCssnext]).process(css).css
+  css = css.replace(/:root/g, ':scope')
+  return css
+}
